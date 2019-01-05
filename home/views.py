8625d2fc9a3da79cb6636a2d116dc0e68764de18
cv <cvaleonline@gmail.com>
2019-01-05 01:32:01 +0100

    cleanup and comments

--- home/views.py
+++ home/views.py
@@ -4,40 +4,46 @@ from tablib import Dataset
 
 web_app = Blueprint('home_view', __name__)
 
+# get path for db
+db_path = os.path.join("/".join(os.path.dirname(__file__).split("/")[:-1]),'db/tweets.csv')
+print(db_path)
 
-@web_app.route('/') # Route for the page
-@web_app.route('/home') # Route for the page
+@web_app.route('/')
+@web_app.route('/home')
 def display_home_page():
+    """Function renders main page showing server status and user options."""
     return render_template('main.html')
 
 
-@web_app.route('/table')  # Route for the page
+@web_app.route('/table')
 def display_table():
+    """Function renders placeholder table page."""
     return render_template('table.html')
 
-@web_app.route('/screen1content.html')
-def json_screen1():
-    path = os.path.dirname(__file__)
 
-    dataset = Dataset().load(open(os.path.join("/".join(path.split("/")[:-1]),'db/tweets.csv')).read())
+@web_app.route('/tablecontent.html')
+def table_content():
+    """Function renders table content."""
+    dataset = Dataset().load(open(db_path).read())
     return render_template('content.html', data=dataset.html)
 
 
-
-@web_app.route('/screen1')  # Route for the page
+@web_app.route('/screen1')
 def display_screen1():
+    """Function renders tweet screen."""
     return render_template('screen1.html')
 
 
-@web_app.route('/screen2')  # Route for the page
+@web_app.route('/screen2')
 def display_screen2():
+    """Function renders tweet screen."""
     return render_template('screen2.html')
 
 
-# ERROR HANDLING
 @web_app.errorhandler(404)  # Not found
 @web_app.errorhandler(403)  # Forbidden
 @web_app.errorhandler(410)  # Gone
 @web_app.errorhandler(500)  # Internal Server Error
 def page_not_found(e):
-    return 'Error encountered: {}'.format(e)
+    """Function for error handling."""
+    return 'Error encountered {}'.format(e)
